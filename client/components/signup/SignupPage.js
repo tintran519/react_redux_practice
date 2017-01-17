7a376fcbc67402e1111f443e451bf1c0f68273d7
Tin Tran <tin.tran0519@gmail.com>
2017-01-16 21:07:51 -0800

    post request and redux installed

--- client/components/signup/SignupPage.js
+++ client/components/signup/SignupPage.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import SignupForm from './SignupForm';
+import { connect } from 'react-redux';
+import { userSignupRequest } from '../../actions/signupActions';
 
 class SignupPage extends React.Component {
   render() {
+    const { userSignupRequest } = this.props
     return (
       <div className="row">
         <div className="col-md-4 col-md-offset-4">
-          <SignupForm />
+          <SignupForm userSignupRequest={userSignupRequest} />
         </div>
       </div>
     );
   }
 }
 
-export default SignupPage;
+SignupPage.propTypes = {
+  userSignupRequest: React.PropTypes.func.isRequired
+}
+
+//connect takes two params; first is mapStateToProps which provides data from redux store
+//takes state and returns object
+//second param is mapDispatchToProps; specifies action creators wrapped in dispatch
+export default connect(null, { userSignupRequest })(SignupPage);
--- client/index.js
+++ client/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 
 import routes from './routes';
 
-render(<Router history={browserHistory} routes={routes} />, document.getElementById('app'));
+//Store takes several params; first one is root reducer(returns new state), second is initial state
+//3rd param allows middleware
+//thunk allows us to dispatch synchronous actions
+const store = createStore(
+  (state = {}) => state,
+  applyMiddleware(thunk)
+);
+
+render(
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>, document.getElementById('app'));
